f5d536bbdf5dee640ed85b40c8693e562d3e89bb
Adam Doussan <doussan.adam@knights.ucf.edu>
2018-12-24 18:29:57 -0500

    updated landing page to use new header

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-class LandingPage extends Component {
-  render() {
+import { withStyles } from '@material-ui/core/styles';
+
+import Header from 'components/Header/Header.js';
+
+import styles from './LandingPageStyles.js';
+
+class LandingPage extends Component
+{
+  render()
+  {
+    const { classes } = this.props;
+
     return (
       <div className="App">
-        Hello, World!
+        <Header active='Home'/>
+        <div className={classes.headerFix}>
+          <p>Hello, World!</p>
+        </div>
       </div>
     );
   }
 }
 
-export default LandingPage;
+export default withStyles(styles)(LandingPage);
---
+++ src/views/LandingPage/LandingPageStyles.js
@@ -0,0 +1,7 @@
+import headerFix from 'assets/jss/headerFix.js';
+
+const styles = {
+  ...headerFix,
+};
+
+export default styles;
