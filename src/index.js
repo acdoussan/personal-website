1ed6192e4f5ab9d192e260ce238cf16ecd13467a
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-01-03 19:46:56 -0500

    renamed cssBaseline to get rid of warning

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import cssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from 'components/Header/Header.js';
 
@@ -38,7 +38,7 @@ const theme = createMuiTheme({
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <cssBaseline />
+    <CssBaseline />
     <Router history={hist}>
       <div>
         <Header/>
