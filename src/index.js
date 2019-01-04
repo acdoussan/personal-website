488d12d02a0d0e2280e87950089bb56898544e8a
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-01-03 19:47:39 -0500

    added breakpoints to theme; only the small breakpoint is changed from the dafault.

--- src/index.js
+++ src/index.js
@@ -34,6 +34,15 @@ const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 650,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
 
 ReactDOM.render(
