76dacd1cac9f95dfba25217fad8580acb9813b92
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-01-03 19:45:51 -0500

    added all stub pages to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
-import LandingPage from 'views/LandingPage/LandingPage.js';
 import AboutMe from 'views/AboutMe/AboutMe.js';
+import LandingPage from 'views/LandingPage/LandingPage.js';
+import Projects from 'views/Projects/Projects.js';
+import Resume from 'views/Resume/Resume.js';
 
 const routes = [
-  { path: "/about", name: "About", component: AboutMe },
-  { path: "/", name: "LandingPage", component: LandingPage },
+  { path: "/about", name: "About Me", component: AboutMe },
+  { path: "/projects", name: "Projects", component: Projects },
+  { path: "/resume", name: "Resume", component: Resume },
+  { path: "/", name: "Landing Page", component: LandingPage },
 ];
 
 export default routes;
