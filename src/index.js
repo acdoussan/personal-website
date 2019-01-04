3fcbb288bfb64e719720e9f145b19ae1891534d6
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-01-03 19:43:50 -0500

    renamed clearStyles to baseStyles; added a base style to set 100% width on inaccessable containing elements

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Header from 'components/Header/Header.js';
 
 import routes from 'routes/index.js';
 
-import 'assets/css/clearStyles.css';
+import 'assets/css/baseStyles.css';
 
 const hist = createBrowserHistory();
 
