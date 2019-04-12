6c05e1c84400dad30078016a9f0262e106519064
Adam Doussan <doussan.adam@knights.ucf.edu>
2019-04-12 12:26:51 -0400

    updated resume component

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 import styles from './ResumeStyles.js';
+
+import Warning from '@material-ui/icons/WarningRounded';
+
+import ResumePDF from 'assets/img/Resume.pdf';
 
 class Resume extends Component
 {
@@ -12,7 +19,18 @@ class Resume extends Component
 
     return (
       <div className={classes.resume}>
-        <p>Resume!</p>
+        <Paper className={classes.paper} elevation={6}>
+          <Typography variant="h3" className={classes.header}>
+            <Warning className={classes.icon} />
+            <span className={classes.title}>Under Construction!</span>
+          </Typography>
+
+          <Typography variant="subtitle1" className={classes.text}>
+            {'For now, check out my resume\u00a0'}
+            <a href={ResumePDF} target='_blank' rel='noopener noreferrer'>here</a>
+            {'.'}
+          </Typography>
+        </Paper>
       </div>
     );
   }
--- src/views/Resume/ResumeStyles.js
+++ src/views/Resume/ResumeStyles.js
@@ -1,4 +1,62 @@
-const styles = {
-};
+const styles = (theme) =>
+{
+  const { breakpoints } = theme;
+
+  return ({
+    resume: {
+      ...theme.flexCenter,
+      width: '100%',
+      height: `calc(100vh - ${theme.headerHeight})`,
+    },
+
+    paper: {
+      padding: '25px',
+
+      [breakpoints.up("md")]: {
+        padding: '50px',
+      },
+
+    },
+
+    icon: {
+      color: '#fac533',
+      backgroundColor: 'black',
+      width: '30px',
+      height: '30px',
+
+      [breakpoints.up("sm")]: {
+        width: '50px',
+        height: '50px',
+      },
+
+      [breakpoints.up("md")]: {
+        width: '75px',
+        height: '75px',
+      },
+    },
+
+    header: {
+      ...theme.flexCenter,
+      fontSize: '20px',
+
+      [breakpoints.up("sm")]: {
+        fontSize: '26px',
+      },
+
+      [breakpoints.up("md")]: {
+        fontSize: '32px',
+      },
+    },
+
+    title: {
+      padding: '0px 20px',
+    },
+
+    text: {
+      ...theme.flexCenter,
+      padding: '10px 0px 0px 0px',
+    },
+  });
+}
 
 export default styles;
